02f5d144c06e67b0f561266af5e44aaafea22931
Mello <dan.mello1001@gmail.com>
2014-11-24 11:11:09 -0700

    completed exercise

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   def index
     @patients = Patient.order(:first_name, :last_name)
     @medications = Medication.order(:name)
+    @prescriptions = Prescription.all
   end
 
 end
